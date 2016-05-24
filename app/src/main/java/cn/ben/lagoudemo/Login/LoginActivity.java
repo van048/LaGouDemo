d77c96edbffbcea4d13e2da15552d4810636d2c8
van048 <Administrator@sgamedev-PC9.tencent.com>
2016-05-24 21:20:13 +0800

    Login Activity basic layout

---
+++ app/src/main/java/cn/ben/lagoudemo/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package cn.ben.lagoudemo.Login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.ben.lagoudemo.R;
+
+public class LoginActivity extends Activity implements LoginView{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/cn/ben/lagoudemo/Login/LoginView.java
@@ -0,0 +1,7 @@
+package cn.ben.lagoudemo.Login;
+
+/**
+ * Created by Administrator on 2016/5/24.
+ */
+public interface LoginView {
+}
